8cbb0cc77090fee9519e462536834201e400c8cd
長谷川雄大 <hasegawayuudai@hasegawayuudainoMacBook-Air.local>
2019-11-04 17:08:31 +0900

    [add]3-7

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -8,9 +8,28 @@ class TodolistsController < ApplicationController
   	list = List.new(list_params)
   	#dbへ保存
   	list.save
-    #トップ画面へリダイレクト
-  	redirect_to '/top'
+    #しょうさい
+  	redirect_to todolist_path(list.id)
   end
+
+  def index
+  	@lists = List.all
+  end
+
+  def show
+  	@list = List.find(params[:id])
+  end
+
+  def edit
+  	@list = List.find(params[:id])
+  end
+
+  def update
+  	list = List.find(params[:id])
+  	  list.update(list_params)
+  	  redirect_to todolist_path(list.id)
+  end
+
   private
 
   def list_params
